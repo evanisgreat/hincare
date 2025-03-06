65b0f1a8dedd37e79e219a7502b150432423cd30
evanisgreat <71905148+evanisgreat@users.noreply.github.com>
2025-03-06 17:38:52 -0600

    Add files via upload

--- hkppltravel/hkppltravel/spiders/hkppltravel.py
+++ hkppltravel/hkppltravel/spiders/hkppltravel.py
@@ -16,15 +16,52 @@ class HKPplTravel(scrapy.Spider):
 
 
     def parselink(self, response):
-        event = {'name': '', 'date': '', 'location': ''}
+        event = {'name': '', 'subcategory activities': [], 'date': [], 'time': [], 'location': [], 'about': '', 'image_link': '', 'website url': ''}
 
-        event['name'] = response.xpath("//h2//text()").get()
+        url = response.xpath("//link[@rel='canonical']/@href").get()
+
+        subheader_names = response.xpath('//div[1]/div/div/main/article/div[3]/div/h2//text()').getall()
+
+        for subheader_name in subheader_names:
+            event['subcategory activities'].append(subheader_name)
+
+        event['website url'] = url
+
+        name1 = response.xpath("//h1//text()").get()
+        name2 = response.xpath("//h2//text()").get()
+
+        print(name1)
+        print(name2)
+
+        if '【' in name1:
+            event['name'] = name1
+        elif '【' in name2:
+            event['name'] = name2
+        else:
+            return
+
         event_text = response.xpath("//p//text()").getall()
 
         for text in event_text:
-            if '日期' in text:
-                event['date'] = text
-            elif '地點' in text:
-                event['location'] = text
+            if '日期：' in text:
+                event['date'].append(text)
+            elif '時間：' in text:
+                event['time'].append(text)
+            elif '地點：' in text or '地址：' in text:
+                event['location'].append(text)
+            else:
+                event['about'] += text + "\n"
+
+        img_link = response.xpath("//figure[@class='wp-block-image size-large']/img/@data-src").get()
+
+        print(img_link)
+
+        event['image_link'] = img_link
+
+        if len(event['date']) == 0:
+            event['date'].append("any time")
+
+        if len(event['time']) == 0:
+            event['time'].append("any time")
 
         yield event
